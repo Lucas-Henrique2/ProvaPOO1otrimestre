d6e12174ffa1661439846c9fa291335db387b9d6
Lucas Henrique. <lucashenriquefernandessoares@gmail.com>
2026-09-17 22:07:20 -0300

    Add files via upload
    ..

---
+++ Padaria.java
@@ -0,0 +1,12 @@
+package br.edu.exemplo.padaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Padaria {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Padaria.class, args);
+    }
+}
